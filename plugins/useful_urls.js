4806754fcb333fdc95bdcdbf4251c9d51011ae47
Wohlstand <admin@wohlnet.ru>
2017-08-07 04:04:34 +0300

    Added SMBX2 PGE update guide url into "Useful URLs" plugin

--- plugins/useful_urls.js
+++ plugins/useful_urls.js
@@ -11,6 +11,11 @@ var lab = function(bot, message, args)
     message.channel.send("http://wohlsoft.ru/docs/_laboratory/").catch(core.msgSendError);
 }
 
+var smbx2pgeUpdate = function(bot, message, args)
+{
+    message.channel.send("http://wohlsoft.ru/forum/viewtopic.php?f=11&t=1977").catch(core.msgSendError);
+}
+
 var repo = function(bot, message, args)
 {
     message.channel.send("https://github.com/WohlSoft/PGE-Project").catch(core.msgSendError);
@@ -27,6 +32,7 @@ function registerCommands(foxyCore)
     core = foxyCore;
     core.addCMD(["testurl",  testUrl,          "Test of URL returning. Just return a WohlSoft site url!"]);
     core.addCMD(["lab",      lab,              "Returns PGE Laboratory URL"]);
+    core.addCMD(["s2p",      smbx2pgeUpdate,   "Returns URL to PGE update guide for SMBX 2"]);
     core.addCMD(["repo",     repo,             "Returns URL to PGE repository on GitHub"]);
     core.addCMD(["markup",   markup,           "Returns URL for a Discord markdown guide"]);
 }
