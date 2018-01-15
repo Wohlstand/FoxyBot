f78cddd9a372d296931be0c6e0a4e88245393fb7
Vitaly Novichkov <admin@wohlnet.ru>
2018-01-15 20:55:27 +0300

    UORGP: Added Renegades role for user joining

--- plugins/disabled/UORPG_things.js
+++ plugins/disabled/UORPG_things.js
@@ -25,24 +25,47 @@ function getOrder(guild)
     return guild.roles.find('name', 'Order');
 }
 
-var joinOrder = function(bot, message, args)
+function getRenegades(guild)
+{
+    return guild.roles.find('name', 'Renegades');
+}
+
+var cleanRoles = function(bot, message, args)
 {
     var chaos = getChaos(message.guild);
     var order = getOrder(message.guild);
+    var renegades = getRenegades(message.guild);
     var member = message.guild.members.get(message.author.id);
+    member.removeRole(order);
     member.removeRole(chaos);
+    member.removeRole(renegades);
+}
+
+var joinOrder = function(bot, message, args)
+{
+    cleanRoles(bot, message, args);
+    var order = getOrder(message.guild);
+    var member = message.guild.members.get(message.author.id);
     member.addRole(order);
     message.reply("Welcome to Order!", core.msgSendError);
 }
 
 var joinChaos = function(bot, message, args)
 {
+    cleanRoles(bot, message, args);
     var chaos = getChaos(message.guild);
-    var order = getOrder(message.guild);
     var member = message.guild.members.get(message.author.id);
-    member.removeRole(order);
     member.addRole(chaos);
     message.reply("Welcome to Chaos!", core.msgSendError);
+}
+
+var joinRenegades = function(bot, message, args)
+{
+    cleanRoles(bot, message, args);
+    var renegades = getRenegades(message.guild);
+    var member = message.guild.members.get(message.author.id);
+    member.addRole(renegades);
+    message.reply("Welcome to Renegades!", core.msgSendError);
 }
 
 var crystalPhase = function(bot, message, args)
@@ -88,6 +111,7 @@ function registerCommands(foxyCore)
     core = foxyCore;
     core.addCMD(["order",      joinOrder,           "Join The Order Alliance - Holy Empire and Insurgents!!!", [], true, [UORPG_Server] ]);
     core.addCMD(["chaos",      joinChaos,           "Join The Chaos Alliance - Army of Darkness and The Shadows!!!", [], true, [UORPG_Server] ]);
+    core.addCMD(["renegades",  joinRenegades,       "Join Renegades - traitors and outcasts; players without alliance.", [], true, [UORPG_Server] ]);
     core.addCMD(["crystal",    crystalPhase,        "Show projection where crystal is located", [], true, [UORPG_Server] ]);
 }
 
@@ -104,14 +128,16 @@ function guildMemberAdd(bot, guildMember)
             "<@" + guildMember.user.id +">, Приветствуем вас на сервере Ultima Online - UORPG.net!\n\n" +
             "Выберите вашу фракцию:\n" +
             "Альянс Порядок - Священная Империя и Повстанцы\n" +
-            "Альянс Хаос - Армия Тьмы и Тени\n\n" +
-            "Для того, чтобы выбрать вашу сторону в войне напишите в чате: /foxy order или /foxy chaos";
+            "Альянс Хаос - Армия Тьмы и Тени\n" +
+            "Ренегаты - предатели и отступники, войны без альянса.\n\n" +
+            "Для того, чтобы выбрать вашу сторону в войне напишите в чате: /foxy order, /foxy chaos или /foxy renegades";
         var message_en =
             "<@" + guildMember.user.id +">, Hi, welcome to Ultima Online server - UORPG.net!\n\n" +
             "Assign your faction please:\n" +
             "The Order Alliance - Holy Empire and Insurgents\n" +
-            "The Chaos Alliance - Army of Darkness and The Shadows\n\n"+
-            "In order to confirm your war force, type: /foxy order ;  /foxy chaos";
+            "The Chaos Alliance - Army of Darkness and The Shadows\n"+
+            "The Renegades - traitors and outcasts; players without alliance.\n\n"+
+            "In order to confirm your war force, type: /foxy order ; /foxy chaos ;  /foxy renegades";
 
         ru.send(message_ru).catch(core.msgSendError);
         en.send(message_en).catch(core.msgSendError);
