a4e160817f1496e23e50f2caac6999656c1cb2fe
Wohlstand <admin@wohlnet.ru>
2021-05-08 20:26:49 +0300

    Careful Foxy: Completely remove the quotes emailing to me
    I no longer need for it. It was very useful while I had many "embassies" of my projects at multiple servers during 2016~2019, however, starting from the moment when I do have my own server for my projects, this function became very annoying. So, since this commit, Foxy no longer emails me anything and no longer puts the letter icon at the ping/mention/reply to me.

--- plugins/careful_foxy.js
+++ plugins/careful_foxy.js
@@ -15,123 +15,123 @@ function hasReg(msg, word)
     return msg.regexIndexOf(word) !== -1;
 }
 
-function hasStr(msg, word)
-{
-    return msg.indexOf(word) !== -1;
-}
+// function hasStr(msg, word)
+// {
+//     return msg.indexOf(word) !== -1;
+// }
 
-let eggCommands = [
-    "$color",
-    "$commander",
-    "$egg",
-    "$hatch",
-    "$ping",
-    "$prefix",
-    "$role",
-    "$tag",
-    "$hang",
-    "$cmd",
-    "$help",
-    "$rps",
-    "$quote", // Dead commands
-    "$talk",
-    "$spam"
-];
+// let eggCommands = [
+//     "$color",
+//     "$commander",
+//     "$egg",
+//     "$hatch",
+//     "$ping",
+//     "$prefix",
+//     "$role",
+//     "$tag",
+//     "$hang",
+//     "$cmd",
+//     "$help",
+//     "$rps",
+//     "$quote", // Dead commands
+//     "$talk",
+//     "$spam"
+// ];
 
 
 // Check out is Yoshi021's Egg bot online, if not - notify user
 // that Egg is "eaten" :-P
-function lookUpForEgg(myBot, message, msgLowTrimmed, allowWrite)
-{
-    if(!allowWrite)
-        return false;
+// function lookUpForEgg(myBot, message, msgLowTrimmed, allowWrite)
+// {
+//     if(!allowWrite)
+//         return false;
+//
+//     if(!message.channel || !message.channel.guild || message.channel.guild.id !== "692079249594515607")
+//         return false; // Don't check egg bot out of a home server
+//
+//     for(let i = 0; i < eggCommands.length; ++i)
+//     {
+//         if(msgLowTrimmed.startsWith(eggCommands[i]))
+//         {
+//             try
+//             {
+//                 let egg = myBot.users.resolve("247080657182785546");
+//                 if(egg && egg.presence.status === "offline")
+//                 {
+//                     message.reply("Sorry, egg is offline... :cooking:", botCommands.msgSendError);
+//                 }
+//             }
+//             catch (e)
+//             {
+//                 botCommands.sendErrorMsg(myBot, message.channel, e);
+//             }
+//             return true;
+//         }
+//     }
+//     return false;
+// }
 
-    if(!message.channel || !message.channel.guild || message.channel.guild.id !== "692079249594515607")
-        return false; // Don't check egg bot out of a home server
+// let keyPrefix = [
+//     "wohl",
+//     "whol",
+//     "wolh",
+//     "wohlstand",
+//     "wholstand",
+//     "wolhstand"
+// ];
 
-    for(let i = 0; i < eggCommands.length; ++i)
-    {
-        if(msgLowTrimmed.startsWith(eggCommands[i]))
-        {
-            try
-            {
-                let egg = myBot.users.resolve("247080657182785546");
-                if(egg && egg.presence.status === "offline")
-                {
-                    message.reply("Sorry, egg is offline... :cooking:", botCommands.msgSendError);
-                }
-            }
-            catch (e)
-            {
-                botCommands.sendErrorMsg(myBot, message.channel, e);
-            }
-            return true;
-        }
-    }
-    return false;
-}
-
-let keyPrefix = [
-    "wohl",
-    "whol",
-    "wolh",
-    "wohlstand",
-    "wholstand",
-    "wolhstand"
-];
-
-function lookUpForKeyPrefix(msgLowTrimmed)
-{
-    var forMe = false;
-
-    for(var i = 0; i < keyPrefix.length; i++)
-    {
-        if((msgLowTrimmed.indexOf(keyPrefix[i] + ":") === 0) ||
-           (msgLowTrimmed.indexOf(keyPrefix[i] + ",") === 0))
-            forMe = true;
-    }
-
-    return forMe;
-}
+// function lookUpForKeyPrefix(msgLowTrimmed)
+// {
+//     var forMe = false;
+//
+//     for(var i = 0; i < keyPrefix.length; i++)
+//     {
+//         if((msgLowTrimmed.indexOf(keyPrefix[i] + ":") === 0) ||
+//            (msgLowTrimmed.indexOf(keyPrefix[i] + ",") === 0))
+//             forMe = true;
+//     }
+//
+//     return forMe;
+// }
 
 // [ \-_+=\\/!@#$%^&*():;'".,?|`~]
 
-const keyMentions = [
-    /\b(?:p[ \-_+=\\/!@#$%^&*():;'".,?|`~]*g[ \-_+=\\/!@#$%^&*():;'".,?|`~]*e)\b/gi,
-    /\b(?:w[ \-_+=\\/!@#$%^&*():;'".,?|`~]*o[ \-_+=\\/!@#$%^&*():;'".,?|`~]*h[ \-_+=\\/!@#$%^&*():;'".,?|`~]*l)\b/gi,
-    /\b(?:w[ \-_+=\\/!@#$%^&*():;'".,?|`~]*h[ \-_+=\\/!@#$%^&*():;'".,?|`~]*o[ \-_+=\\/!@#$%^&*():;'".,?|`~]*l)\b/gi,
-    /\b(?:w[ \-_+=\\/!@#$%^&*():;'".,?|`~]*o[ \-_+=\\/!@#$%^&*():;'".,?|`~]*l[ \-_+=\\/!@#$%^&*():;'".,?|`~]*h)\b/gi,
-    /w(ohl|hol|olh)e?stand/gi,
-    /\b(?:moon[ \-_+=\\/!@#$%^&*():;'".,?|`~]*dust)\b/gi,
-    /\b(?:adl[ \-_+=\\/!@#$%^&*():;'".,?|`~]*midi)\b/gi,
-    /\b(?:opn[ \-_+=\\/!@#$%^&*():;'".,?|`~]*midi)\b/gi,
-    /\b(?:sdl2?)\b/gi,
-    /\b(?:sdl2?[ \-_+=\\/!@#$%^&*():;'".,?|`~]*mixer[ \-_+=\\/!@#$%^&*():;'".,?|`~]*?e?x?t?)\b/gi,
-    /\b(?:mixer[ \-_+=\\/!@#$%^&*():;'".,?|`~]*x)\b/gi,
-    /\b(?:midi)\b/gi
-];
+// const keyMentions = [
+//     /\b(?:p[ \-_+=\\/!@#$%^&*():;'".,?|`~]*g[ \-_+=\\/!@#$%^&*():;'".,?|`~]*e)\b/gi,
+//     /\b(?:w[ \-_+=\\/!@#$%^&*():;'".,?|`~]*o[ \-_+=\\/!@#$%^&*():;'".,?|`~]*h[ \-_+=\\/!@#$%^&*():;'".,?|`~]*l)\b/gi,
+//     /\b(?:w[ \-_+=\\/!@#$%^&*():;'".,?|`~]*h[ \-_+=\\/!@#$%^&*():;'".,?|`~]*o[ \-_+=\\/!@#$%^&*():;'".,?|`~]*l)\b/gi,
+//     /\b(?:w[ \-_+=\\/!@#$%^&*():;'".,?|`~]*o[ \-_+=\\/!@#$%^&*():;'".,?|`~]*l[ \-_+=\\/!@#$%^&*():;'".,?|`~]*h)\b/gi,
+//     /w(ohl|hol|olh)e?stand/gi,
+//     /\b(?:moon[ \-_+=\\/!@#$%^&*():;'".,?|`~]*dust)\b/gi,
+//     /\b(?:adl[ \-_+=\\/!@#$%^&*():;'".,?|`~]*midi)\b/gi,
+//     /\b(?:opn[ \-_+=\\/!@#$%^&*():;'".,?|`~]*midi)\b/gi,
+//     /\b(?:sdl2?)\b/gi,
+//     /\b(?:sdl2?[ \-_+=\\/!@#$%^&*():;'".,?|`~]*mixer[ \-_+=\\/!@#$%^&*():;'".,?|`~]*?e?x?t?)\b/gi,
+//     /\b(?:mixer[ \-_+=\\/!@#$%^&*():;'".,?|`~]*x)\b/gi,
+//     /\b(?:midi)\b/gi
+// ];
 
-function hasMatches(text, reg)
-{
-    let m = text.match(reg);
-    return m && m.length > 0;
-}
+// function hasMatches(text, reg)
+// {
+//     let m = text.match(reg);
+//     return m && m.length > 0;
+// }
 
-function lookUpForKeyMentions(msgLowTrimmed)
-{
-    let forMe = false;
-
-    for(let i = 0; i < keyMentions.length; i++)
-    {
-        if(hasMatches(msgLowTrimmed, keyMentions[i]))
-        {
-            botCommands.foxyLogInfo("== Caught a mention of " + keyMentions[i] + "! ==");
-            forMe = true;
-        }
-    }
-
-    return forMe;
-}
+// function lookUpForKeyMentions(msgLowTrimmed)
+// {
+//     let forMe = false;
+//
+//     for(let i = 0; i < keyMentions.length; i++)
+//     {
+//         if(hasMatches(msgLowTrimmed, keyMentions[i]))
+//         {
+//             botCommands.foxyLogInfo("== Caught a mention of " + keyMentions[i] + "! ==");
+//             forMe = true;
+//         }
+//     }
+//
+//     return forMe;
+// }
 
 function messageIn(mybot, message, allowWrite)
 {
@@ -155,22 +155,22 @@ function messageIn(mybot, message, allowWrite)
     let messageForMeReact = false;
     let mentions = message.mentions.users.array();
 
-    for(let i = 0; i < mentions.length; i++)
-    {
-        botCommands.foxyLogInfo( "---> " + mentions[i].username + "#" + mentions[i].discriminator);
-        wasAsked = (mentions[i].id === mybot.user.id);
-        messageForMe = (mentions[i].id === "182039820879659008") && (message.author.id !== mybot.user.id);
-        messageForMeReact = messageForMe;
-    }
-
-    if(lookUpForKeyMentions(msgLowTrimmed)) //Shadow email
-        messageForMe = true;
-
-    if(lookUpForKeyPrefix(msgLowTrimmed))   //Transparent email
-    {
-        messageForMe = true;
-        messageForMeReact = true;
-    }
+    // for(let i = 0; i < mentions.length; i++)
+    // {
+    //     botCommands.foxyLogInfo( "---> " + mentions[i].username + "#" + mentions[i].discriminator);
+    //     wasAsked = (mentions[i].id === mybot.user.id);
+    //     messageForMe = (mentions[i].id === "182039820879659008") && (message.author.id !== mybot.user.id);
+    //     messageForMeReact = messageForMe;
+    // }
+    //
+    // if(lookUpForKeyMentions(msgLowTrimmed)) //Shadow email
+    //     messageForMe = true;
+    //
+    // if(lookUpForKeyPrefix(msgLowTrimmed))   //Transparent email
+    // {
+    //     messageForMe = true;
+    //     messageForMeReact = true;
+    // }
 
 //        var whoWannaPing = [69055500540456960/*Spinda*/];
 //        if( (whoWannaPing.indexOf(message.author.id) != -1)
@@ -185,21 +185,22 @@ function messageIn(mybot, message, allowWrite)
 //            else
 //                message.reply("disable notifications please! :hear_no_evil:");
 //        }
-    if(message.author.id === "182039820879659008")//Don't quote me, Foxy!!!
-        messageForMe = false;
 
-    if(message.author.id === "303661490114789391")//Don't disturb me, Yappy Bot
-        messageForMe = false;
-
-    if(botCommands.isurl(msgTrimmed))//Also please, don't report me URLs
-        messageForMe = false;
-
-    if((message.author.id === "216273975939039235") && messageForMe)
-    {
-        messageForMeReact = false; //Don't react to LunaBot
-        if((msgLowTrimmed.indexOf("http://wohlsoft.ru/") === 0) && (msgLowTrimmed.indexOf(" ") === -1))
-            messageForMe = false; //Don't report LunaBot's URLs
-    }
+    // if(message.author.id === "182039820879659008")//Don't quote me, Foxy!!!
+    //     messageForMe = false;
+    //
+    // if(message.author.id === "303661490114789391")//Don't disturb me, Yappy Bot
+    //     messageForMe = false;
+    //
+    // if(botCommands.isurl(msgTrimmed))//Also please, don't report me URLs
+    //     messageForMe = false;
+    //
+    // if((message.author.id === "216273975939039235") && messageForMe)
+    // {
+    //     messageForMeReact = false; //Don't react to LunaBot
+    //     if((msgLowTrimmed.indexOf("http://wohlsoft.ru/") === 0) && (msgLowTrimmed.indexOf(" ") === -1))
+    //         messageForMe = false; //Don't report LunaBot's URLs
+    // }
 
     //Check is botane offline, and reply on attempt call her
     let Botane = mybot.users.resolve("216688100032643072");
@@ -211,16 +212,16 @@ function messageIn(mybot, message, allowWrite)
         }
     }
 
-    if(allowWrite && (wasAsked || message.channel.isPrivate))
+    if(allowWrite && (wasAsked || message.channel.type === 'dm'))
     {
-        if(message.author.id === "216688100032643072")//Horikawa Botane
+        if(message.author.id === "216688100032643072") // Horikawa Botane
         {
             if(msgLow.indexOf("dorkatron") !== -1)
             {
                 message.reply("maybe you are a Dorkatron? I'm not!");
             }
         }
-        else//Any other
+        else // Any other
         {
             if(msgLow.indexOf("pets") !== -1)
             {
@@ -281,39 +282,39 @@ function messageIn(mybot, message, allowWrite)
     }
     else
     {
-        if(messageForMe && message.guild)
-        {
-            // Check if Me is on this guild
-            message.guild.members.fetch("182039820879659008").then(function (gotMember)
-            {
-                // Check if Me can see this channel
-                if(message.channel.permissionsFor(gotMember).has('VIEW_CHANNEL'))
-                {
-                    console.log("Sending email...");
-                    botCommands.sendEmail(message, message.content, false);
-                    if(messageForMeReact)
-                    {
-                        message.react("📧").then(function (q)
-                        {
-                            console.log("Reaction was set");
-                        });//Mark message as reported
-                    }
-                }
-                else
-                {
-                    console.log("Got a keyword mention from inaccessible channel");
-                }
-            }).catch(function(err)
-            {
-                console.log("Got a keyword mention from inaccessible guild");
-            });
-        }
+        // if(messageForMe && message.guild)
+        // {
+        //     // Check if Me is on this guild
+        //     message.guild.members.fetch("182039820879659008").then(function (gotMember)
+        //     {
+        //         // Check if Me can see this channel
+        //         if(message.channel.permissionsFor(gotMember).has('VIEW_CHANNEL'))
+        //         {
+        //             console.log("Sending email...");
+        //             botCommands.sendEmail(message, message.content, false);
+        //             if(messageForMeReact)
+        //             {
+        //                 message.react("📧").then(function (q)
+        //                 {
+        //                     console.log("Reaction was set");
+        //                 });//Mark message as reported
+        //             }
+        //         }
+        //         else
+        //         {
+        //             console.log("Got a keyword mention from inaccessible channel");
+        //         }
+        //     }).catch(function(err)
+        //     {
+        //         console.log("Got a keyword mention from inaccessible guild");
+        //     });
+        // }
 
         if(allowWrite)
         {
-            if(botCommands.botConfig.defaultChannel.includes(message.channel.id))//"beep-boop", "fun" 218194030662647809
+            if(botCommands.botConfig.defaultChannel.includes(message.channel.id)) // "beep-boop", "fun" 218194030662647809
             {
-                if(message.author.id === 216688100032643072)//Horikawa Botane
+                if(message.author.id === "216688100032643072")//Horikawa Botane
                 {
                     if(msgLowTrimmed.indexOf("is it porn?") !== -1)
                     {
